79b06fd3e262f3775fae9de32924ace526ea2c29
Julius0594 <158364861+JuliusM94@users.noreply.github.com>
2024-06-14 22:20:45 -0400

    Update #2
    - Added a textbox, await function and basic styling.
    - I only have 2 definitions
    - Still trying to add pronounciation

--- public/assets/javascript/myscript.js
+++ public/assets/javascript/myscript.js
@@ -1,1 +1,43 @@
 console.log("Welcome to, Merriam-Webster Dictionary API!");
+
+
+
+
+
+// API Key = 46ac3ec6-4aa6-447c-977a-765d26a646d5
+
+async function getData() {
+    const result = await fetch ("https://www.dictionaryapi.com/api/v3/references/collegiate/json/dragon?key=46ac3ec6-4aa6-447c-977a-765d26a646d5")
+    const infoFromServer = await result.json();
+
+    console.log('info from server: ' + JSON.stringify(infoFromServer))
+}
+
+getData();
+
+//<br>${pronounciation}</br>
+
+async function getDefinition() {
+    const word = document.getElementById('textInput').value;
+    const url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=46ac3ec6-4aa6-447c-977a-765d26a646d5`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        let myResult = document.getElementById('result');
+        myResult.innerHTML = '';
+
+        if (data.length > 0) {
+            const definition = data[0].shortdef[0];
+            const anotherDefinition = data[0].shortdef[1];
+            // const pronounciation = audio[0];
+            myResult.innerHTML = `<h2>Here's the definition you are looking for the word, "${word}"</h2><br>${anotherDefinition}</br><br>${definition}</br>`;
+        } else {
+            myResult.innerHTML = '<p>No definition found.</p>';
+        }
+    } catch (error) {
+        console.error('Error fetching the definition:', error);
+        document.getElementById('result').innerHTML = '<p>Error fetching the definition.</p>';
+    }
+}
